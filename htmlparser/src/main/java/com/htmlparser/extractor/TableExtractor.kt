4173267be710b76db353d82fc1f65057d6464bea
Ibrahim Ali <eng.ibrahim222@gmail.com>
2020-11-26 12:19:57 +0200

    fix table extractor

--- htmlparser/src/main/java/com/htmlparser/extractor/TableExtractor.kt
+++ htmlparser/src/main/java/com/htmlparser/extractor/TableExtractor.kt
@@ -6,19 +6,16 @@ import com.htmlparser.model.Table
 class TableExtractor(val element: Element) : Extractor<Table>(element) {
     override fun extract(): Table {
         val tableList = ArrayList<List<String>>()
-        for (allElement in element.children()) {
-            val list = ArrayList<String>()
-            for (element in allElement.children()) {
-                // go in deep one more level if it's built with <thead & <tbody
-                if (element.childrenSize() > 1) {
-                    for (ele in element.children()) {
-                        addToList(element, list)
-                    }
-                } else {
-                    addToList(element, list)
+        for (ele in element.allElements) {
+            if (ele.tagName() == "tr") {
+                val list = ArrayList<String>()
+                for (child in ele.children()) {
+                    addToList(child, list)
                 }
+                tableList.add(list)
+                continue
             }
-            tableList.add(list)
+
         }
         return Table(tableList)
     }
@@ -27,7 +24,7 @@ class TableExtractor(val element: Element) : Extractor<Table>(element) {
         element: Element,
         list: ArrayList<String>
     ) {
-        if (element.hasAttr("th") || element.hasAttr("td") ||
+        if (element.hasAttr("th") || element.tagName() == "td" ||
             element.hasClass("th") || element.tagName() == "tr" || element.tagName() == "th"
         ) {
             list.add(element.text())
